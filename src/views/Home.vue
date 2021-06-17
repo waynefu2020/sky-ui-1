7140a8797076f39f816002b6207c57c2b2d6e129
DDWayne <652507565@qq.com>
2021-06-17 11:59:10 +0800

    topnav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="topNav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="banner">
       <h1>木林森UI</h1>
@@ -21,3 +24,23 @@ export default {
 
 }
 </script>
+
+<style lang="scss" scoped>
+  .topNav{
+    background: pink;
+    display: flex;
+    padding: 16px;
+    > .logo{
+      max-width: 6em;
+      margin-right: auto;
+    }
+    > .menu{
+      display: flex;
+      white-space: nowrap;
+      flex-wrap: nowrap;
+      > li{
+        margin: 0 1em;
+      }
+    }
+  }
+</style>
