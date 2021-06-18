e35adf32029f9aa9a8d8ed52ceb565ea4ff4e99d
DDWayne <652507565@qq.com>
2021-06-18 16:54:30 +0800

    基本完成网站框架

---
+++ src/components/ButtonDemo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>ButtonDemo</div>
+</template>
+
+<script lang="ts">
+
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/DocDemo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>默认页面</div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
