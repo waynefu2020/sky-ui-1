1c809f6fa143d7a11bebe1edd43f11d3108ef68b
DDWayne <652507565@qq.com>
2021-06-18 15:00:54 +0800

    封装Topnav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <div class="topNav">
-      <div class="logo">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+    <Topnav/>
     <div class="banner">
       <h1>木林森UI</h1>
       <h2>清新极简的UI框架</h2>
@@ -18,29 +12,13 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 
+import Topnav from "../components/Topnav.vue";
 export default {
-
+  components: {Topnav}
 }
 </script>
 
 <style lang="scss" scoped>
-  .topNav{
-    background: pink;
-    display: flex;
-    padding: 16px;
-    > .logo{
-      max-width: 6em;
-      margin-right: auto;
-    }
-    > .menu{
-      display: flex;
-      white-space: nowrap;
-      flex-wrap: nowrap;
-      > li{
-        margin: 0 1em;
-      }
-    }
-  }
 </style>
