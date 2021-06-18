f40aa383c2ab911a9774dd2ace9bf322bfff7a5b
DDWayne <652507565@qq.com>
2021-06-18 17:48:26 +0800

    hover之后右滚

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,32 +1,14 @@
 <template>
   <div>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
-    <p>Switch文档</p>
+    <Switch/>
   </div>
 </template>
 
 <script lang="ts">
+import Switch from '../lib/Switch.vue';
 
 export default {
-
+  components:{Switch}
 }
 </script>
 
---
+++ src/lib/Switch.vue
@@ -0,0 +1,35 @@
+<template>
+  <button>
+    <span></span>
+  </button>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h*2;
+  border: none;
+  background: blue;
+  border-radius: $h/2;
+  position: relative;
+
+  span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    background: white;
+    height: $h2;
+    width: $h2;
+    border-radius: $h2/2;
+  }
+}
+button:hover > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+</style>
