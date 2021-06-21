282f7d94170fe535b93e1817c93dd659bd91cbe1
DDWayne <652507565@qq.com>
2021-06-21 10:24:03 +0800

    完成button基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,7 +2,10 @@
   <div>Button 示例</div>
   <h1>示例1</h1>
   <div>
-    <Button>我是按钮</Button>
+    <Button>我是默认按钮</Button>
+    <Button theme="button">我是按钮</Button>
+    <Button theme="link">我是链接</Button>
+    <Button theme="text">我是文本</Button>
   </div>
 </template>
 
@@ -11,7 +14,13 @@
 
 import Button from '../lib/Button.vue';
 export default {
-  components: {Button}
+  components: {Button},
+  setup(){
+    const onClick = ()=>{
+      console.log('你好');
+    }
+    return {onClick}
+  }
 }
 </script>
 
