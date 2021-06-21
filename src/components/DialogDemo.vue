53b2591a236cb6dc96336e204aed78352d959c86
DDWayne <652507565@qq.com>
2021-06-21 16:55:35 +0800

    Dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -3,12 +3,11 @@
     <div>Dialog示例</div>
     <h1>示例1</h1>
     <Button @click="toggle">Toggle</Button>
-    <Dialog :visible="x"></Dialog>
+    <Dialog v-model:visible="x" :close-on-click-overlay="false" :ok="f1" :cancel="f2"></Dialog>
   </div>
 </template>
 
 <script lang="ts">
-
 
 import Dialog from '../lib/Dialog.vue';
 import Button from '../lib/Button.vue';
@@ -20,7 +19,11 @@ export default {
     const toggle = ()=>{
       x.value = !x.value
     }
-    return {x, toggle}
+    const f1 = () =>{
+      return false
+    }
+    const f2 = () =>{}
+    return {x, toggle, f1,f2}
   }
 }
 </script>
