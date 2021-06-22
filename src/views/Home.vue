b1fe66eb966af903afa31bad1281781d1f77027c
DDWayne <652507565@qq.com>
2021-06-22 12:29:44 +0800

    添加banner圆弧

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,6 +41,7 @@ $color: #0a9f98;
 
 .topNavAndBanner{
   background: linear-gradient(148deg, rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
+  clip-path: ellipse(80% 60% at 50% 40%);
 }
 .features{
   > svg{
