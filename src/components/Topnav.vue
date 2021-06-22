f5474395cb2f5325bdc87ee4a550603ec080b7bd
DDWayne <652507565@qq.com>
2021-06-22 12:25:15 +0800

    引入首页icon

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="topNav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <div class="logo" >
+      <svg class="icon" @click="toggleMenu">
+        <use xlink:href="#icon-tree"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -24,7 +28,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$color: #0a9f98;
+
 .topNav {
+  color: $color;
   display: flex;
   padding: 16px;
   position: fixed;
@@ -37,6 +44,10 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    > svg{
+      width: 24px;
+      height: 24px;
+    }
   }
 
   > .menu {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="topNavAndBanner">
-    <Topnav/>
-    <div class="banner">
-      <h1>木林森UI</h1>
-      <h2>清新极简的UI框架</h2>
-      <p class="actions">
-        <a href="https://github.com/waynefu2020/tree-ui-1">Github</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+  <div>
+    <div class="topNavAndBanner">
+      <Topnav/>
+      <div class="banner">
+        <h1>木林森UI</h1>
+        <h2>清新极简的UI框架</h2>
+        <p class="actions">
+          <a href="https://github.com/waynefu2020/tree-ui-1">Github</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
+    </div>
+    <div class="features">
+      <svg class="icon">
+        <use xlink:href="#icon-vue"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-ts"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-light"></use>
+      </svg>
     </div>
   </div>
+
 </template>
 
 <script lang="ts">
@@ -21,10 +35,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$green: #02bcb0;
+$radius: 4px;
+$color: #0a9f98;
+
 .topNavAndBanner{
   background: linear-gradient(148deg, rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
 }
+.features{
+  > svg{
+    width: 64px;
+    height: 64px;
+  }
+}
   .banner{
+    color: $color;
     padding: 100px 0;
     display: flex;
     justify-content: center;
@@ -34,13 +59,14 @@ export default {
       padding: 8px 0;
       a{
         margin: 0 8px;
-        background: #fff;
+        background: $green;
+        color: white;
         display: inline-block;
-        $h: 28px;
-        height: $h;
-        line-height: $h;
-        border-radius: $h/2;
-        padding: 0 8px;
+        border-radius: $radius;
+        padding: 8px 22px;
+        &:hover{
+          text-decoration: none;
+        }
       }
     }
   }
