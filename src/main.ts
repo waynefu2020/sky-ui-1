3235c1c6fbb5df5694e4eed16eb3658cbcae8d20
DDWayne <652507565@qq.com>
2021-06-22 20:05:42 +0800

    抽离用户需要拷贝的Switch代码

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import './lib/tree.scss'
-import './index.scss'
-import { createApp } from 'vue'
-import App from './App.vue'
+import './lib/tree.scss';
+import './index.scss';
+import {createApp} from 'vue';
+import App from './App.vue';
 import {router} from './router';
-import 'github-markdown-css'
+import 'github-markdown-css';
 import Markdown from './components/Markdown.vue';
 
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
-app.component('Markdown',Markdown)
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
+app.component('Markdown', Markdown);
--- src/router.ts
+++ src/router.ts
@@ -2,19 +2,16 @@ import {createRouter, createWebHashHistory} from 'vue-router';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
 import SwitchDemo from './components/SwitchDemo.vue';
-import ButtonDemo from './components/ButtonDemo.vue'
-import DialogDemo from './components/DialogDemo.vue'
-import TabsDemo from './components/TabsDemo.vue'
-import DocDemo from './components/DocDemo.vue'
-import Intro from './views/Intro.vue'
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
+import ButtonDemo from './components/ButtonDemo.vue';
+import DialogDemo from './components/DialogDemo.vue';
+import TabsDemo from './components/TabsDemo.vue';
+import DocDemo from './components/DocDemo.vue';
 import {h} from 'vue';
-import Markdown from './components/Markdown.vue'
+import Markdown from './components/Markdown.vue';
 
 
 const history = createWebHashHistory();
-const md = filename => h(Markdown, {path:`../markdown/${filename}.md`, key: filename})
+const md = filename => h(Markdown, {path: `../markdown/${filename}.md`, key: filename});
 export const router = createRouter({
     history: history,
     routes: [
