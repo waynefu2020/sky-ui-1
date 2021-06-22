ec7c63d3272c81a7ad2e756c58a54eee5da6da53
DDWayne <652507565@qq.com>
2021-06-22 14:59:26 +0800

    官网首页features样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,15 +12,32 @@
       </div>
     </div>
     <div class="features">
-      <svg class="icon">
-        <use xlink:href="#icon-vue"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-ts"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-light"></use>
-      </svg>
+      <ul>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>采用了Composition API</p>
+        </li>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript </h3>
+          <p>源代码采用 TypeScript 书写（非严格检查）</p>
+        </li>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-light"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的代码都力求简洁</p>
+        </li>
+      </ul>
+
+
+
     </div>
   </div>
 
@@ -44,10 +61,44 @@ $color: #0a9f98;
   clip-path: ellipse(80% 60% at 50% 40%);
 }
 .features{
-  > svg{
-    width: 64px;
-    height: 64px;
+  margin: 64px auto;
+  width: 400px;
+
+  @media (min-width: 800px) {
+    width: 800px;
   }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  > ul{
+    display: flex;
+    flex-wrap: wrap;
+    > li{
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+          "icon title"
+          "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      > svg{
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+      > h3{
+        grid-area: title;
+        font-size: 28px;
+      }
+      >p{
+        grid-area: text;
+      }
+    }
+  }
+
 }
   .banner{
     color: $color;
