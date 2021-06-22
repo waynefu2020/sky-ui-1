c96afa0a0f939fc6cb30367824f6250f045adfd7
DDWayne <652507565@qq.com>
2021-06-22 20:31:10 +0800

    展示源代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+        <pre>{{Switch1Demo.__sourceCode}}</pre>
       </div>
     </div>
     <div class="demo">
@@ -19,7 +19,7 @@
         <Switch2Demo/>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+        <pre>{{Switch2Demo.__sourceCode}}</pre>
       </div>
     </div>
   </div>
@@ -36,7 +36,7 @@ export default {
   components:{Switch2Demo, Switch1Demo, Switch,Button},
   setup(){
     const bool = ref(false)
-    return {bool}
+    return {bool,Switch1Demo,Switch2Demo}
   }
 }
 </script>
